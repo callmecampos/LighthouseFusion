4770efd9d3bc6cdcac54c5323d018d30ea90b043
alexmore214 <alexmore214@gmail.com>
2018-12-05 03:11:03 -0800

    code for arduino

---
+++ PoseSerialCapture/PoseSerialCapture.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(500000);
+  int i=0;
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+    Serial.print("0 1 1 1 1 1 2\n");
+}
