079f7672723f7b209204eb5bdfc7741a26b37ce8
callmecampos <felipemrcampos@gmail.com>
2018-12-04 22:01:19 -0800

    json realsense params upload for live capture

--- bag_to_klg.py
+++ bag_to_klg.py
@@ -15,7 +15,7 @@ import pyrealsense2 as rs
 import numpy as np
 # Import OpenCV for easy image rendering
 import cv2
-import os, traceback, sys, argparse
+import os, traceback, sys, argparse, json
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -29,8 +29,27 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
+
+
+DS5_product_ids = ["0AD1", "0AD2", "0AD3", "0AD4", "0AD5", "0AF6", "0AFE", "0AFF", "0B00", "0B01", "0B03", "0B07"]
+
+def find_device_that_supports_advanced_mode() :
+    ctx = rs.context()
+    ds5_dev = rs.device()
+    devices = ctx.query_devices();
+    print(devices)
+    for dev in devices:
+        print(dev.supports(rs.camera_info.product_id))
+        print(str(dev.get_info(rs.camera_info.product_id)))
+        if dev.supports(rs.camera_info.product_id) and str(dev.get_info(rs.camera_info.product_id)) in DS5_product_ids:
+            if dev.supports(rs.camera_info.name):
+                print("Found device that supports advanced mode:", dev.get_info(rs.camera_info.name))
+            return dev
+    raise Exception("No device that supports advanced mode was found")
+
 def main():
     # Streaming loop
+    valid_try = False
     try:
         if not os.path.exists(args.directory):
             os.mkdir(args.directory)
@@ -53,6 +72,36 @@ def main():
             rs.config.enable_device_from_file(config, args.input, False)
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+        if not args.live:
+            #Enabling advanced mode to load settings from JSON file
+            dev = find_device_that_supports_advanced_mode()
+            advnc_mode = rs.rs400_advanced_mode(dev)
+            print("Advanced mode is", "enabled" if advnc_mode.is_enabled() else "disabled")
+
+            # Loop until we successfully enable advanced mode
+            while not advnc_mode.is_enabled():
+                print("Trying to enable advanced mode...")
+                advnc_mode.toggle_advanced_mode(True)
+                # At this point the device will disconnect and re-connect.
+                print("Sleeping for 5 seconds...")
+                time.sleep(5)
+                # The 'dev' object will become invalid and we need to initialize it again
+                dev = find_device_that_supports_advanced_mode()
+                advnc_mode = rs.rs400_advanced_mode(dev)
+                print("Advanced mode is", "enabled" if advnc_mode.is_enabled() else "disabled")
+
+
+            # Loading controls from a json string
+            # For Python 2, the values in 'as_json_object' dict need to be converted from unicode object to utf-8
+            with open(sys.path[0]+"/realsense_params.json") as f:
+                as_json_object = json.load(f)
+            if type(next(iter(as_json_object))) != str:
+                as_json_object = {k.encode('utf-8'): v.encode("utf-8") for k, v in as_json_object.items()}
+            # The C++ JSON parser requires double-quotes for the json object so we need
+            # to replace the single quote of the pythonic json to double-quotes
+            json_string = str(as_json_object).replace("'", '\"')
+            advnc_mode.load_json(json_string)
 
         # Start streaming
         profile = pipeline.start(config)
@@ -123,18 +172,21 @@ def main():
 
             if (i % 20 == 0): # reset running average frame-rate params
                 t0, i0 = time(), i
+
+            valid_try = True
     finally:
         cv2.destroyAllWindows()
         if args.traceback:
             traceback.print_exc()
 
-        with cd(sys.path[0] + '/png_to_klg/build'): # generate the .klg file
-            os.system('./pngtoklg -w ' + args.directory + '/ -o ' + args.directory + '/realsense.klg')
+        if valid_try:
+            with cd(sys.path[0] + '/png_to_klg/build'): # generate the .klg file
+                os.system('./pngtoklg -w ' + args.directory + '/ -o ' + args.directory + '/realsense.klg')
 
-        if args.elastic: # if flag enabled, run Elastic Fusion on the generated .klg
-            print('Running ElasticFusion...')
-            with cd(sys.path[0] + '/ElasticFusion/GUI/build'):
-                os.system('./ElasticFusion -l ' + args.directory + '/realsense.klg')
+            if args.elastic: # if flag enabled, run Elastic Fusion on the generated .klg
+                print('Running ElasticFusion...')
+                with cd(sys.path[0] + '/ElasticFusion/GUI/build'):
+                    os.system('./ElasticFusion -l ' + args.directory + '/realsense.klg')
 
 
 if __name__ == "__main__":
