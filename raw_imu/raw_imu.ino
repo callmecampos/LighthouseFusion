04c0cad66b26b05a620958158111220b61a328ea
alexmore214 <alexmore214@gmail.com>
2018-12-09 21:52:01 -0800

    added position estimate for IMU and cleaned up python code

--- raw_imu/raw_imu.ino
+++ raw_imu/raw_imu.ino
@@ -18,20 +18,28 @@
 */
 
 /* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (33)
+#define BNO055_SAMPLERATE_DELAY_MS (1)
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
+
 
 /**************************************************************************/
 /*
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
+const int ledPin=13; //
+signed long sstatex, sstatey, sstatez;
+signed int accelerationx[]={0,0}, accelerationy[]={0,0}, accelerationz[]={0,0};
+signed long velocityx[]={0,0}, velocityy[]={0,0}, velocityz[]={0,0};
+signed long positionX[]={0,0}, positionY[]={0,0}, positionZ[]={0,0};
+unsigned char countx, county, countz;
+void movement_end_check(void);
 void setup(void)
 {
   Serial.begin(9600);
   Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
-
+  pinMode(ledPin,OUTPUT);
   /* Initialise the sensor */
   if(!bno.begin())
   {
@@ -52,8 +60,25 @@ void setup(void)
 
   Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
     /* Display calibration status for each sensor. */
-  uint8_t system, gyro, accel, mag = 0;
+
   Serial.println("x, y, z, qx, qy, qz, qw, gyro, acce, mag ");
+  //Calibration for Earth's Gravity
+  // NO MOVEMENT Required
+   unsigned int count1;
+   count1 = 0;
+
+   do{
+  //Linear Acceleration Data
+   imu::Vector<3> lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+   sstatex = sstatex + 100*lin.x(); // Accumulate Samples
+   sstatey = sstatey + 100*lin.y();
+   sstatez = sstatez + 100*lin.z();
+   count1++;
+   }while(count1!=0x0400); // 1024 times
+   sstatex=sstatex>>10; // division between 1024
+   sstatey=sstatey>>10;
+   sstatez=sstatez>>10;
+   digitalWrite(ledPin,HIGH);
 }
 
 /**************************************************************************/
@@ -71,16 +96,134 @@ void loop(void)
   // - VECTOR_EULER         - degrees
   // - VECTOR_LINEARACCEL   - m/s^2
   // - VECTOR_GRAVITY       - m/s^2
-  
+  float LPFrequency =.5;
+  float HPFrequency =30;
   uint8_t system, gyro, accel, mag = 0;
-  //Linear Acceleration Data
-  imu::Vector<3> lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  unsigned char count2 ;
+  count2=0;
+
+  imu::Vector<3> lin;
+  imu::Quaternion quat;
+  while(gyro==0 || accel==0 || mag==0){
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    Serial.printf("%i, %i, %i\n",gyro,accel,mag);
+  }
   
-  // Quaternion Data
-  imu::Quaternion quat = bno.getQuat();
+  do{
+  lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    
+   // Quaternion Data
+  quat = bno.getQuat();
   bno.getCalibration(&system, &gyro, &accel, &mag);
   
+  accelerationx[1]=accelerationx[1] + lin.x(); //filtering routine for noise attenuation
+  accelerationy[1]=accelerationy[1] + lin.y(); //64 samples are averaged. The resulting 
+  accelerationz[1]=accelerationz[1] + lin.z(); //filtering routine for noise attenuation
+  
+  
+  //average represents the acceleration of
+  //an instant
+  count2++;
+  
+  }while (count2!=0x40); // 64 sums of the acceleration sample
+  
+  
+  accelerationx[1]= accelerationx[1]>>6; // division by 64
+  accelerationy[1]= accelerationy[1]>>6;
+  accelerationz[1]= accelerationz[1]>>6;
+  
+  accelerationx[1] = accelerationx[1] - (int)sstatex; //eliminating zero reference
+   //offset of the acceleration data
+  accelerationy[1] = accelerationy[1] - (int)sstatey; // to obtain positive and negative
+   //acceleration
+  accelerationz[1] = accelerationz[1] - (int)sstatez;
+  
+  if ((accelerationx[1] <=3)&&(accelerationx[1] >= -3)&&(accelerationz[1] >= -3)) //Discrimination window applied
+   {accelerationx[1] = 0;} // to the X axis acceleration
+   //variable
+  if ((accelerationy[1] <=3)&&(accelerationy[1] >= -3)&&(accelerationz[1] >= -3))
+    {accelerationy[1] = 0;}
+  if ((accelerationz[1] <=3)&&(accelerationx[1] >= -3)&&(accelerationy[1] >= -3)) //Discrimination window applied
+    {accelerationz[1] = 0;} // to the X axis acceleration
+    
+   //first X integration:
+  velocityx[1]= velocityx[0]+ accelerationx[0]+ ((accelerationx[1] -accelerationx[0])>>1);
+   //second X integration:
+  positionX[1]= positionX[0] + velocityx[0] + ((velocityx[1] - velocityx[0])>>1);
+   //first Y integration:
+  velocityy[1] = velocityy[0] + accelerationy[0] + ((accelerationy[1] -accelerationy[0])>>1);
+   //second Y integration:
+  positionY[1] = positionY[0] + velocityy[0] + ((velocityy[1] - velocityy[0])>>1);
+  
+     //first Z integration:
+  velocityz[1] = velocityz[0] + accelerationz[0] + ((accelerationz[1] -accelerationz[0])>>1);
+   //second Z integration:
+  positionZ[1] = positionZ[0] + velocityz[0] + ((velocityz[1] - velocityz[0])>>1);
+  
+  accelerationx[0] = accelerationx[1]; //The current acceleration value must be sent
+  //to the previous acceleration
+  accelerationy[0] = accelerationy[1]; //variable in order to introduce the new
+  //acceleration value.
+  accelerationz[0] = accelerationz[1];
+  
+  velocityx[0] = velocityx[1]; //Same done for the velocity variable
+  velocityy[0] = velocityy[1];
+  velocityz[0] = velocityz[1];
+  
+  Serial.printf("% 08ld, % 08ld, % 08ld,\t% 015.12f, % 015.12f, % 015.12f, % 015.12f,\t %i, %i, %i\n",positionX[1],positionY[1],positionZ[1],quat.x(),quat.y(),quat.z(),quat.w(),gyro,accel,mag);
+
+
+
+//  positionX[1] = positionX[1]<<18; //The idea behind this shifting (multiplication)
+//   //is a sensibility adjustment.
+//  positionY[1] = positionY[1]<<18; //Some applications require adjustments to a
+//  positionZ[1] = positionZ[1]<<18; //particular situation
+//   //i.e. mouse application
+//  data_transfer();
+//  
+//  positionX[1] = positionX[1]>>18; //once the variables are sent them must return to
+//  positionY[1] = positionY[1]>>18; //their original state
+  movement_end_check();
+
+  positionX[0] = positionX[1]; //actual position data must be sent to the
+  positionY[0] = positionY[1]; //previous position
+  positionZ[0] = positionZ[1];
+  //direction = 0; // data variable to direction variable reset 
+
   /* Display the floating point data */
-  Serial.printf("% 08.1f, % 08.1f, % 08.1f\t% 015.12f, % 015.12f, % 015.12f, % 015.12f,\t %i, %i, %i\n",100*lin.x(),100*lin.y(),100*lin.z(),quat.x(),quat.y(),quat.z(),quat.w(),gyro,accel,mag);
+
   delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+void movement_end_check(void)
+{
+  if (accelerationx[1]==0) //we count the number of acceleration samples that equals cero
+  { countx++;}
+  else { countx =0;}
+
+  if (countx>=25) //if this number exceeds 25, we can assume that velocity is cero
+  {
+    velocityx[1]=0;
+    velocityx[0]=0;
+  }
+
+  if (accelerationy[1]==0) //we do the same for the Y axis
+  { county++;}
+  else { county =0;}
+
+  if (county>=25)
+  {
+    velocityy[1]=0;
+    velocityy[0]=0;
+  }
+ 
+  if (accelerationz[1]==0) //we do the same for the Y axis
+  {countz++;}
+  else { countz =0;}
+
+  if (countz>=25)
+  {
+    velocityz[1]=0;
+    velocityz[0]=0;
+  }
 }
--- serialto.py
+++ serialto.py
@@ -1,27 +1,42 @@
 # native python stuff
 from __future__ import print_function
 from time import time
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
 
 
 # Import Numpy for easy array manipulation
 import numpy as np
 # Import OpenCV for easy image rendering
-#import cv2
+import cv2
 import os, traceback, sys, argparse, json, io
 
 import serial 
 ser = serial.Serial('COM4',9600, timeout=1) 
 pose_queue = list()
+pose_queue_floats =list()
+
+sampling_rate= 30;
 try:
-    i=0
-    while i<10:
-    
-        pose_queue.insert(i,ser.readline())
-        i+=1
-    i=0
-    while i<10:
-    
-        print(pose_queue[i])   
-        i+=1
+	cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+	i=0
+	j=0
+	while True:
+		pose_queue.insert(i,ser.readline())
+		# print (pose_queue[i])
+		
+	#	while j<10:
+
+		pose_queue_floats= map(float, pose_queue[i].split(','))
+		print(pose_queue_floats[0])
+		print(pose_queue_floats[1])
+		print(pose_queue_floats[2])
+		key = cv2.waitKey(1)
+		# Press esc or 'q' to close the image window
+		if key & 0xFF == ord('q') or key == 27:
+			break
+		i+=1	
+	# for pose in pose_queue:
+		# print(pose)
 finally:
     print("done")
