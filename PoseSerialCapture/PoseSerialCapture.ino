dc5fe1a454f2755b1260ee97232cdadc5ebc0f7e
alexmore214 <alexmore214@gmail.com>
2018-12-10 13:19:55 -0800

    minor chnages

--- PoseSerialCapture/PoseSerialCapture.ino
+++ PoseSerialCapture/PoseSerialCapture.ino
@@ -2,10 +2,16 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(500000);
   int i=0;
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
     Serial.print("0 1 1 1 1 1 2\n");
+    digitalWrite(13, HIGH);
+    delay(1000);
+    Serial.print("1 0 1 0 1 0 1\n");
+    digitalWrite(13,LOW);
+    delay(1000);
 }
--- raw_imu/raw_imu.ino
+++ raw_imu/raw_imu.ino
@@ -19,9 +19,14 @@
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (1)
-
+#define average (5)
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
+
+uint8_t sy = 0;
+uint8_t gyro=0, accel=0, mag = 0;
+unsigned char count2=0;
+unsigned int count1=0;
 
 /**************************************************************************/
 /*
@@ -29,11 +34,12 @@ Adafruit_BNO055 bno = Adafruit_BNO055();
 */
 /**************************************************************************/
 const int ledPin=13; //
-signed long sstatex, sstatey, sstatez;
-signed int accelerationx[]={0,0}, accelerationy[]={0,0}, accelerationz[]={0,0};
-signed long velocityx[]={0,0}, velocityy[]={0,0}, velocityz[]={0,0};
-signed long positionX[]={0,0}, positionY[]={0,0}, positionZ[]={0,0};
-unsigned char countx, county, countz;
+float sstatex, sstatey, sstatez;
+float accelerationx[]={0.0,0.0}, accelerationy[]={0.0,0.0}, accelerationz[]={0.0,0.0};
+float velocityx[]={0,0}, velocityy[]={0,0}, velocityz[]={0,0};
+float positionX[]={0,0}, positionY[]={0,0}, positionZ[]={0,0};
+unsigned char countx=0, county=0, countz=0;
+float time = .001*BNO055_SAMPLERATE_DELAY_MS*average;
 void movement_end_check(void);
 void setup(void)
 {
@@ -62,23 +68,9 @@ void setup(void)
     /* Display calibration status for each sensor. */
 
   Serial.println("x, y, z, qx, qy, qz, qw, gyro, acce, mag ");
-  //Calibration for Earth's Gravity
-  // NO MOVEMENT Required
-   unsigned int count1;
-   count1 = 0;
 
-   do{
-  //Linear Acceleration Data
-   imu::Vector<3> lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-   sstatex = sstatex + 100*lin.x(); // Accumulate Samples
-   sstatey = sstatey + 100*lin.y();
-   sstatez = sstatez + 100*lin.z();
-   count1++;
-   }while(count1!=0x0400); // 1024 times
-   sstatex=sstatex>>10; // division between 1024
-   sstatey=sstatey>>10;
-   sstatez=sstatez>>10;
-   digitalWrite(ledPin,HIGH);
+
+
 }
 
 /**************************************************************************/
@@ -96,26 +88,44 @@ void loop(void)
   // - VECTOR_EULER         - degrees
   // - VECTOR_LINEARACCEL   - m/s^2
   // - VECTOR_GRAVITY       - m/s^2
-  float LPFrequency =.5;
-  float HPFrequency =30;
-  uint8_t system, gyro, accel, mag = 0;
-  unsigned char count2 ;
-  count2=0;
 
+  count2 = 0;
+ 
   imu::Vector<3> lin;
   imu::Quaternion quat;
-  while(gyro==0 || accel==0 || mag==0){
-    bno.getCalibration(&system, &gyro, &accel, &mag);
-    lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-    Serial.printf("%i, %i, %i\n",gyro,accel,mag);
+  if (gyro==0 || accel==0 || mag==0){
+    digitalWrite(ledPin,LOW);
+    while(gyro==0 || accel==0 || mag==0){
+      bno.getCalibration(&sy, &gyro, &accel, &mag);
+      lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+      Serial.printf("%i, %i, %i\n",gyro,accel,mag);
+    }
+
+    Serial.printf("CALIBRATED Part A wait 5 seconds");
+    delay(5000);
+    //Calibration for Earth's Gravity
+    // NO MOVEMENT Required
+    count1=0;
+    do{
+       //Linear Acceleration Data
+       imu::Vector<3> lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+       sstatex = sstatex + lin.x(); // Accumulate Samples
+       sstatey = sstatey + lin.y();
+       sstatez = sstatez + lin.z();
+       count1++;
+     }while(count1!=1024); // 1024 times
+     sstatex=sstatex/1024; // division between 1024
+     sstatey=sstatey/1024;
+     sstatez=sstatez/1024;
+     digitalWrite(ledPin,HIGH);
+     Serial.printf("CALIBRATED Part B wait 5 seconds");  
   }
-  
   do{
   lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
     
-   // Quaternion Data
-  quat = bno.getQuat();
-  bno.getCalibration(&system, &gyro, &accel, &mag);
+//   // Quaternion Data
+//  quat = bno.getQuat();
+//  bno.getCalibration(&sy, &gyro, &accel, &mag);
   
   accelerationx[1]=accelerationx[1] + lin.x(); //filtering routine for noise attenuation
   accelerationy[1]=accelerationy[1] + lin.y(); //64 samples are averaged. The resulting 
@@ -126,40 +136,41 @@ void loop(void)
   //an instant
   count2++;
   
-  }while (count2!=0x40); // 64 sums of the acceleration sample
+  }while (count2!=average); // 64 sums of the acceleration sample
   
   
-  accelerationx[1]= accelerationx[1]>>6; // division by 64
-  accelerationy[1]= accelerationy[1]>>6;
-  accelerationz[1]= accelerationz[1]>>6;
+  accelerationx[1]= accelerationx[1]/average; // division by 64
+  accelerationy[1]= accelerationy[1]/average;
+  accelerationz[1]= accelerationz[1]/average;
   
-  accelerationx[1] = accelerationx[1] - (int)sstatex; //eliminating zero reference
+  accelerationx[1] = accelerationx[1] - sstatex; //eliminating zero reference
    //offset of the acceleration data
-  accelerationy[1] = accelerationy[1] - (int)sstatey; // to obtain positive and negative
+  accelerationy[1] = accelerationy[1] - sstatey; // to obtain positive and negative
    //acceleration
-  accelerationz[1] = accelerationz[1] - (int)sstatez;
-  
-  if ((accelerationx[1] <=3)&&(accelerationx[1] >= -3)&&(accelerationz[1] >= -3)) //Discrimination window applied
+  accelerationz[1] = accelerationz[1] - sstatez;
+//  Serial.printf("%f %f %f\n", accelerationx.06[1], accelerationy[1], accelerationz[1] );
+Serial.printf("% 08.3f, % 08.3f, % 08.3f,\t% 015.12f, % 015.12f, % 015.12f, % 015.12f,\t %i, %i, %i\n",accelerationx[1],accelerationy[1],accelerationz[1],quat.x(),quat.y(),quat.z(),quat.w(),gyro,accel,mag);
+  if ((accelerationx[1] <=.1)&&(accelerationx[1] >= -.1)) //Discrimination window applied
    {accelerationx[1] = 0;} // to the X axis acceleration
    //variable
-  if ((accelerationy[1] <=3)&&(accelerationy[1] >= -3)&&(accelerationz[1] >= -3))
+  if ((accelerationy[1] <=.1)&&(accelerationy[1] >= -.1))
     {accelerationy[1] = 0;}
-  if ((accelerationz[1] <=3)&&(accelerationx[1] >= -3)&&(accelerationy[1] >= -3)) //Discrimination window applied
+  if ((accelerationz[1] <=.1)&&(accelerationz[1] >= -.1)) //Discrimination window applied
     {accelerationz[1] = 0;} // to the X axis acceleration
     
    //first X integration:
-  velocityx[1]= velocityx[0]+ accelerationx[0]+ ((accelerationx[1] -accelerationx[0])>>1);
+  velocityx[1]= velocityx[0]+ (accelerationx[0]+ ((accelerationx[1] -accelerationx[0])/2))*time;
    //second X integration:
-  positionX[1]= positionX[0] + velocityx[0] + ((velocityx[1] - velocityx[0])>>1);
+  positionX[1]= positionX[0] + (velocityx[0] + ((velocityx[1] - velocityx[0])/2))*time;
    //first Y integration:
-  velocityy[1] = velocityy[0] + accelerationy[0] + ((accelerationy[1] -accelerationy[0])>>1);
+  velocityy[1] = velocityy[0] + (accelerationy[0] + ((accelerationy[1] -accelerationy[0])/2))*time;
    //second Y integration:
-  positionY[1] = positionY[0] + velocityy[0] + ((velocityy[1] - velocityy[0])>>1);
+  positionY[1] = positionY[0] + (velocityy[0] + ((velocityy[1] - velocityy[0])/2))*time;
   
      //first Z integration:
-  velocityz[1] = velocityz[0] + accelerationz[0] + ((accelerationz[1] -accelerationz[0])>>1);
+  velocityz[1] = velocityz[0] + (accelerationz[0] + ((accelerationz[1] -accelerationz[0])/2))*time;
    //second Z integration:
-  positionZ[1] = positionZ[0] + velocityz[0] + ((velocityz[1] - velocityz[0])>>1);
+  positionZ[1] = positionZ[0] + (velocityz[0] + ((velocityz[1] - velocityz[0])/2))*time;
   
   accelerationx[0] = accelerationx[1]; //The current acceleration value must be sent
   //to the previous acceleration
@@ -171,8 +182,8 @@ void loop(void)
   velocityy[0] = velocityy[1];
   velocityz[0] = velocityz[1];
   
-  Serial.printf("% 08ld, % 08ld, % 08ld,\t% 015.12f, % 015.12f, % 015.12f, % 015.12f,\t %i, %i, %i\n",positionX[1],positionY[1],positionZ[1],quat.x(),quat.y(),quat.z(),quat.w(),gyro,accel,mag);
-
+  Serial.printf("% 08.3f, % 08.3f, % 08.3f,\t% 015.12f, % 015.12f, % 015.12f, % 015.12f,\t %i, %i, %i\n",positionX[1],positionY[1],positionZ[1],quat.x(),quat.y(),quat.z(),quat.w(),gyro,accel,mag);
+  
 
 
 //  positionX[1] = positionX[1]<<18; //The idea behind this shifting (multiplication)
@@ -201,8 +212,9 @@ void movement_end_check(void)
   { countx++;}
   else { countx =0;}
 
-  if (countx>=25) //if this number exceeds 25, we can assume that velocity is cero
+  if (countx>=10) //if this number exceeds 5, we can assume that velocity is cero
   {
+    Serial.printf("clearing out x velocity\n");
     velocityx[1]=0;
     velocityx[0]=0;
   }
@@ -211,7 +223,7 @@ void movement_end_check(void)
   { county++;}
   else { county =0;}
 
-  if (county>=25)
+  if (county>=10)
   {
     velocityy[1]=0;
     velocityy[0]=0;
@@ -221,7 +233,7 @@ void movement_end_check(void)
   {countz++;}
   else { countz =0;}
 
-  if (countz>=25)
+  if (countz>=10)
   {
     velocityz[1]=0;
     velocityz[0]=0;
