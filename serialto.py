541a075909662e0c9c75ac19f5d7d078fc6c8288
alexmore214 <alexmore214@gmail.com>
2018-12-10 17:02:55 -0800

    added threading for serial listening

--- serialto.py
+++ serialto.py
@@ -10,33 +10,56 @@ import numpy as np
 # Import OpenCV for easy image rendering
 import cv2
 import os, traceback, sys, argparse, json, io
+import threading
+import Queue
+import time
 
 import serial 
-ser = serial.Serial('COM4',9600, timeout=1) 
-pose_queue = list()
-pose_queue_floats =list()
+ser = serial.Serial('COM4',9600, timeout=1)
+whereUat = Queue.Queue()
 
-sampling_rate= 30;
+
+sampling_rate= 30
+class worker: 
+    def __init__(self,whereUat,ser): 
+        self.whereUat = whereUat 
+        self.ser = ser 
+
+    def read_serial(self,ser): 
+        while True : 
+            rx=ser.readline()
+            #byte = inp.encode('hex')
+            self.whereUat.put(rx)
+            time.sleep(1)
+
+    def go(self): 
+        th1 = threading.Thread(target=self.read_serial, args=[ser]) 
+        th1.start() 
+
+class worker2: 
+    def __init__(self,whereUat,): 
+        self.whereUat = whereUat 
+        self.ser = ser 
+
+    def getrx(self,whereUat): 
+        while True:
+            rx = self.whereUat.get()
+            print (str(rx))
+
+    def go2(self): 
+        th2 = threading.Thread(target=self.getrx, args=[whereUat])
+        th2.start() 
+
 try:
-	cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-	i=0
-	j=0
-	while True:
-		pose_queue.insert(i,ser.readline())
-		# print (pose_queue[i])
-		
-	#	while j<10:
-
-		pose_queue_floats= map(float, pose_queue[i].split(','))
-		print(pose_queue_floats[0])
-		print(pose_queue_floats[1])
-		print(pose_queue_floats[2])
-		key = cv2.waitKey(1)
-		# Press esc or 'q' to close the image window
-		if key & 0xFF == ord('q') or key == 27:
-			break
-		i+=1	
-	# for pose in pose_queue:
-		# print(pose)
+    #cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+    t = worker(whereUat,ser)
+    t.go()
+    t2 = worker2(whereUat) 
+    t2.go2()
+    time.sleep(10)
+    sys.exit()    
+        # if key & 0xFF == ord('q') or key == 27:
+            
+            # break
 finally:
     print("done")
